2c1b2a67ad3fec6dc5d409f123b707c7c85acc2c
saidamir101 <saidamir101@gmail.com>
2024-01-02 17:41:49 +0500

    add

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+
+
+<script setup>
+
+</script>
+
+<template>
+  
+  <div>
+<div><p class="text-center text-4xl mt-4 font-bold font-mono">{{ title }}</p></div>
+
+<div class="">
+  <div class="justify-center  mt-10 flex gap-5">
+  <input type="" v-model="text" placeholder="I can do" class="outline-none w-[420px] h-[40px] rounded-lg px-3 border border-green-500 ">
+  <button @click="addTodo(id++)" class="bg-green-400 w-[100px] text-white h-[40px] rounded-lg" >Add</button>
+</div>
+  <div class="bg-gray-100  border-red-400 border rounded-lg px-2 w-[35%]  mt-2 mx-auto  " v-for="(todo,) in todos" :key="todo.id">
+    <div class="flex justify-center items-center p-2 gap-10 ">  
+      <span class="todo__id bg-orange-300  text-center px-2  rounded-xl"  >{{ todo.id }}</span>
+  
+      <span    class="todo__text " :class="{todo__text_isDone:isDone}" >{{ todo.text }}</span>
+      
+    </div>
+ 
+
+  </div>
+
+</div>
+  </div>
+</template>
+<script>
+import {mapActions} from "vuex"
+export default {
+  data(){
+    return {
+      title:"Todo List",
+      text:"",
+      todos:[],
+      isDone:false,
+      id:0,
+      editting:false,
+      editText:""
+    }
+  },
+  methods:{
+    ...mapActions(["deleteTodo","editTodo"]),
+    addTodo(){
+      if(this.text.trim().length == 0){
+        alert("please add todo")
+      }
+      else{
+           this.todos.push({
+        id:this.id,
+        text:this.text,
+        done:this.isDone
+    }),
+      this.text=""
+      }
+   
+    },
+    
+ 
+    
+  }
+}
+</script>
+<style lang="scss">
+
+
+</style>
+
